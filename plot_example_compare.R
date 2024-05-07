4b582d0f8a1c99ba8e66353ff6e8c4e65e43a7eb
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2024-05-07 09:40:38 -0400

    Updated the example plot

--- plot_example_compare.R
+++ plot_example_compare.R
@@ -1,9 +1,6 @@
-
-################
-### On the simulated dataset
-################
-
 set.seed(123)
+library(splines)
+library(ggplot2)
 
 # Define the number of observations
 n <- 500
@@ -16,83 +13,64 @@ x <- runif(n)
 A <- rbinom(n, size = 1, prob = 0.5)
 
 # Define the coefficients
-beta0 <- 2
+beta0 <- 1
 beta1 <- 5
-beta2 <- -1
+beta2 <- 1
 beta3 <- 5.5
 
 
 # Generate random error terms
-epsilon <- rnorm(n, mean = 0, sd = 0.25)
+epsilon <- rnorm(n, mean = 0, sd = 0.3)
 
-Y <- beta0 + beta1 * (x-0.5)^3 + beta2 * A + beta3 * A * (x-0.5)^3 + epsilon
+Y <- beta0 + beta1 * (x-0.5)^3 + beta2 * A + beta3 * A * (x-0.5)^3 + A*(x > 0.5)*(x - 0.5)^3 + epsilon
 
 
 DATA_full <- data.frame('Y' = Y, 'x' = x, 'A' = A, 'x.t' = A *x)
 DATA_reduced <- data.frame('Y' = Y, 'x' = x, 'A' = A)
 
+bs_fit <- lm(Y ~ bs(x, df=5) + A*bs(x, df=5), data=DATA_full)
+test_points <- data.frame(x=rep(seq(0.01, 0.99, length.out=500), 2), A=rep(c(0, 1), each=500))
+test_points$fitted_vals <- predict(bs_fit, newdata=test_points)
 
-Y <- DATA_full$Y
-#DATA_full <- DATA_full[ , !(names(DATA_full) %in% c('Y'))]
-#DATA_full <- model.matrix(Y~.-1, data = DATA_full)
 
-
-Treat <- DATA_reduced$A
-DATA_reduced <- DATA_reduced[ , !(names(DATA_reduced) %in% c('Y', 'A'))]
-DATA_reduced <- model.matrix(Y~.-1, data = as.data.frame(DATA_reduced))
-tau <- mean(Y[Treat == 1]) - mean(Y[Treat == 0])
+tau <- mean(Y[A == 1]) - mean(Y[A == 0])
 tau.range <- seq(-4*tau, 4*tau, length.out = 20)
-
-
-
-#data.all <- cbind.data.frame(DATA_full, Y = Y)
-fit <- lm(Y ~., data = DATA_full)
-pred <- predict(fit)
-
-plot_data <- data.frame(coef = DATA_full$x, Observed = Y, Predicted = pred, Trt = Treat)
-# Subset dataframe based on Trt values
-df_trt1 <- plot_data[plot_data$Trt == 1, ]
-df_trt0 <- plot_data[plot_data$Trt == 0, ]
-
 
 mse <- rep(NA, length(tau.range))
 for(k in 1:length(tau.range)) {
-  data.reduced <- cbind.data.frame(DATA_reduced, Y= Y - tau.range[k]*Treat)
-  lm_tmp <- lm(Y ~., data = data.reduced)
-  mse[k] <- mean((data.reduced$Y - lm_tmp$fitted)^2)
+  tau.tmp <- tau.range[k]
+  data.reduced <- data.frame('Y' = Y - tau.tmp*A, 'x' = x)
+  lm_tmp <- lm(Y ~ bs(x), data = data.reduced)
+  ghat <- lm_tmp$fitted.values + tau.tmp*A
+  mse[k] <- mean((Y - ghat)^2)
 }
 tau.star <- tau.range[which.min(mse)]
 
-data.reduced <- cbind.data.frame(DATA_reduced, Y= Y - tau.star*Treat)
-fit_reduced <- lm(Y ~., data = data.reduced)
-pred_reduced <- predict(fit_reduced) + tau.star*Treat
+data.reduced <- data.frame('Y' = Y - tau.star*A, 'x' = x)
+lm_star <- lm(Y ~ bs(x), data = data.reduced)
 
-plot_data_reduced <- data.frame(coef = data.reduced$DATA_reduced, Observed = Y, Predicted = pred_reduced, Trt = Treat)
-# Subset dataframe based on Trt values
-df_reduced_trt1 <- plot_data_reduced[plot_data_reduced$Trt == 1, ]
-df_reduced_trt0 <- plot_data_reduced[plot_data_reduced$Trt == 0, ]
+test_points_reduced0 <- test_points_reduced1 <- data.frame(x=rep(seq(0.01, 0.99, length.out=500), 2))
+test_points_reduced0$fitted_vals <- predict(lm_star, newdata=test_points)
+test_points_reduced1$fitted_vals <- test_points_reduced0$fitted_vals + tau.star
+
+df_trt1 <- subset(DATA_full, A==1)
+df_trt0 <- subset(DATA_full, A==0)
+fit_trt1 <- subset(test_points, A==1)
+fit_trt0 <- subset(test_points, A==0)
 
 
-
-library(ggplot2)
-ggplot() +
-  geom_line(data = df_trt1, aes(x = coef, y = pred), color = "blue") +
-  #geom_point(data = df_reduced_trt1, aes(x = Predicted, y = Observed), color = "blue", alpha = 0.5) +
-  
-  # Add df_trt0 as a line with points from df_reduced_trt0 around it
-  geom_line(data = df_trt0, aes(x = coef, y = pred), color = "red") +
-  #geom_point(data = df_reduced_trt0, aes(x = Predicted, y = Observed), color = "red", alpha = 0.5) +
-  
-  # Add labels to the lines
-  geom_text(data = df_trt1[1, ], aes(x = Predicted, y = Observed, label = "Treat = 1"), hjust = -1.2, vjust = 0.2, color = "blue") +
-  geom_text(data = df_trt0[1, ], aes(x = Predicted, y = Observed, label = "Treat = 0"), hjust = 3.0, vjust = -5.5, color = "red") +
-  
-  # Customize plot aesthetics
-  labs(y = "Predicted", x = "Observed", color = "Treatment") +
-  scale_color_manual(values = c("blue", "red"), labels = c("Trt = 1", "Trt = 0")) +
+ggplot() + geom_point(data = df_trt1, aes(x = x, y = Y), color = "blue", alpha = 0.5) +
+  geom_point(data = df_trt0, aes(x = x, y = Y), color = "red", alpha = 0.5) +
+  geom_line(data = fit_trt1, aes(x = x, y = fitted_vals), color = "blue", alpha = 0.5) +
+  geom_line(data = fit_trt0, aes(x = x, y = fitted_vals), color = "red", alpha = 0.5) +
+  geom_line(data = test_points_reduced1, aes(x = x, y = fitted_vals), color = "blue", alpha = 0.5,
+            linetype=2) +
+  geom_line(data = test_points_reduced0, aes(x = x, y = fitted_vals), color = "red", alpha = 0.5,
+            linetype=2) +
+  geom_text(data = data.frame(x=0.0, Y=2.8), aes(x = x, y = Y, label = "Treatment = 1"),
+            hjust = -0.5, vjust = 0.2, color = "blue") +
+  geom_text(data = data.frame(x=0.0, Y=2.6), aes(x = x, y = Y, label = "Treatment = 0"),
+            hjust = -0.5, vjust = 0.2, color = "red") +
+  labs(y = "Outcome", x = "x", color = "Treatment") +
+  scale_color_manual(values = c("blue", "red"), labels = c("Treatment = 1", "Trt = 0")) +
   theme_minimal()
-
-
-
-
-
