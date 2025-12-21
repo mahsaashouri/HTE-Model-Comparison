215168a0a06699a40ba4520790f77c5ce5b60e83
Mahsa Ashouri <mah.ashouri62@gmail.com>
2025-12-21 12:01:25 -0500

    Updated sim2 by Nick codes

--- Simulation2/SimTwoRunParallel.R
+++ Simulation2/SimTwoRunParallel.R
@@ -7,8 +7,7 @@ library(randomForest)
 set.seed(123567)
 
 ## Source fitting and nested cv functions
-setwd("/Users/ashourm/Projects/HTE-Model-Comparison")  ## Change for your computer
-#setwd("~/Documents/HTEevaluation/HTE-Model-Comparison")
+setwd("~/Projects/HTE-Model-Comparison")
 source("CoreFunctions/CVDiffFunctions.R")
 source("CoreFunctions/FitterFunctions.R")
 source("Simulation2/OneSimTwoRun.R")
@@ -21,26 +20,28 @@ num_cores <- detectCores() - 1  # Use all cores except one
 
 ## change muvec and thetavec if you don't want to 
 ## run as many simulation settings
-muvec <- rep(1, 4)
-thetavec <- rep(1:4)
-
+#muvec <- rep(1:3, each=4)
+#thetavec <- rep(1:4, 3)
+muvec <- rep(3,4)
+thetavec <- 1:4
+rfrun <- FALSE
 # Set up the cluster
 for(j in 1:length(muvec)) {
   cl <- makeCluster(num_cores)
-
+  
   # Export the necessary objects to the cluster
   clusterExport(cl, c("OneSimTwoRun", "mu", "theta", "f1", "f2", "f3", "f4", "cv.glmnet", "glmboost",
                       "randomForest", "predict.glmnet", "predict.glmboost", "Gaussian",
                       "fitter_lm", "fitter_glmnet", "fitter_glmboost", "fitter_rf", "squared_loss", "mse",
                       "predictor_lm", "predictor_glmnet", "predictor_glmboost", "predictor_rf",
                       "nested_cv", "nested_cv_helper", "naive_cv", "linear_regression_funs",
-                      "glmnet_funs", "glmboost_funs", "rf_funs", "muvec", "thetavec", "j"))
-
+                      "glmnet_funs", "glmboost_funs", "rf_funs", "muvec", "thetavec", "j", "rfrun"))
+  
   # Run the simulation
   ## first argument in rep(500, 500) is sample size
   ## second argument in rep(500, 500) is number of simulation replications
-  results <- parLapply(cl, rep(500, 500), function(n) { OneSimTwoRun(n, mu_choice=muvec[j], theta_choice=thetavec[j], p=9, n_folds=5, nested_cv_reps=50) } )
-
+  results <- parLapply(cl, rep(5000, 500), function(n) { OneSimTwoRun(n, mu_choice=muvec[j], theta_choice=thetavec[j], p=9, n_folds=5, nested_cv_reps=50, ranfor=rfrun) } )
+  
   cover_lm <- cover_glmnet <- cover_glmboost <- cover_rf <- rep(NA, length(results))
   hvalue_lm <- hvalue_glmnet <- hvalue_glmboost <- hvalue_rf <- rep(NA, length(results))
   hvalue1_lm <- hvalue1_glmnet <- hvalue1_glmboost <- hvalue1_rf <- rep(NA, length(results))
@@ -50,26 +51,27 @@ for(j in 1:length(muvec)) {
     cover_lm[k] <- results[[k]]$cover_lm
     cover_glmnet[k] <- results[[k]]$cover_glmnet
     cover_glmboost[k] <- results[[k]]$cover_glmboost
-    cover_rf[k] <- results[[k]]$cover_rf
-
+    
     CI_lm[k,] <- results[[k]]$CI_lm
     CI_glmnet[k,] <- results[[k]]$CI_glmnet
     CI_glmboost[k,] <- results[[k]]$CI_glmboost
-    CI_rf[k,] <- results[[k]]$CI_rf
-
+    
     hvalue_lm[k] <- results[[k]]$hvalue_lm
     hvalue1_lm[k] <- results[[k]]$hvalue1side_lm
     hvalue_glmnet[k] <- results[[k]]$hvalue_glmnet
     hvalue1_glmnet[k] <- results[[k]]$hvalue1side_glmnet
     hvalue_glmboost[k] <- results[[k]]$hvalue_glmboost
     hvalue1_glmboost[k] <- results[[k]]$hvalue1side_glmboost
-    hvalue_rf[k] <- results[[k]]$hvalue_rf
-    hvalue1_rf[k] <- results[[k]]$hvalue1side_rf
-
+    if(rfrun) {
+      cover_rf[k] <- results[[k]]$cover_rf
+      CI_rf[k,] <- results[[k]]$CI_rf
+      hvalue_rf[k] <- results[[k]]$hvalue_rf
+      hvalue1_rf[k] <- results[[k]]$hvalue1side_rf
+    }
     TrueThetas[k,] <- results[[k]]$true_thetas
   }
   ## Change file name here:
-  fname <- paste("/Users/ashourm/Projects/HTE-Result", muvec[j],"Theta",thetavec[j],".RData", sep="")
+  fname <- paste("~/Projects/HTE-Model-Comparison/Simulation2/Result", muvec[j],"Theta",thetavec[j],"n5000.RData", sep="")
   save(cover_lm, cover_glmnet, cover_glmboost, cover_rf, CI_lm, CI_glmnet,
        CI_glmboost, CI_rf, hvalue_lm, hvalue_glmnet, hvalue_glmboost, hvalue_rf,
        hvalue1_lm, hvalue1_glmnet, hvalue1_glmboost, hvalue1_rf, TrueThetas,
