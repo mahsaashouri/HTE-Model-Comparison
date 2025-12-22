8c6bdfbd0f3cc96c0710a742705eb45272c78430
Mahsa Ashouri <mah.ashouri62@gmail.com>
2025-12-22 11:05:03 -0500

    Added first round of Sim2 results

--- Simulation2/SimTwoRunParallel.R
+++ Simulation2/SimTwoRunParallel.R
@@ -40,7 +40,7 @@ for(j in 1:length(muvec)) {
   # Run the simulation
   ## first argument in rep(500, 500) is sample size
   ## second argument in rep(500, 500) is number of simulation replications
-  results <- parLapply(cl, rep(5000, 500), function(n) { OneSimTwoRun(n, mu_choice=muvec[j], theta_choice=thetavec[j], p=9, n_folds=5, nested_cv_reps=50, ranfor=rfrun) } )
+  results <- parLapply(cl, rep(1000, 500), function(n) { OneSimTwoRun(n, mu_choice=muvec[j], theta_choice=thetavec[j], p=9, n_folds=5, nested_cv_reps=50, ranfor=rfrun) } )
   
   cover_lm <- cover_glmnet <- cover_glmboost <- cover_rf <- rep(NA, length(results))
   hvalue_lm <- hvalue_glmnet <- hvalue_glmboost <- hvalue_rf <- rep(NA, length(results))
@@ -71,7 +71,7 @@ for(j in 1:length(muvec)) {
     TrueThetas[k,] <- results[[k]]$true_thetas
   }
   ## Change file name here:
-  fname <- paste("~/Projects/HTE-Model-Comparison/Simulation2/Result", muvec[j],"Theta",thetavec[j],"n5000.RData", sep="")
+  fname <- paste("~/Projects/HTE-Model-Comparison/Simulation2/Result", muvec[j],"Theta",thetavec[j],"n1000.RData", sep="")
   save(cover_lm, cover_glmnet, cover_glmboost, cover_rf, CI_lm, CI_glmnet,
        CI_glmboost, CI_rf, hvalue_lm, hvalue_glmnet, hvalue_glmboost, hvalue_rf,
        hvalue1_lm, hvalue1_glmnet, hvalue1_glmboost, hvalue1_rf, TrueThetas,
