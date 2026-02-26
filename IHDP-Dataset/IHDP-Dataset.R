40c361a27b46c9823e556e0b74dd4049154b4f0f
Mahsa Ashouri <mah.ashouri62@gmail.com>
2026-02-26 13:59:04 -0500

    Added the results for nrep500-IHDP dataset

--- IHDP-Dataset/IHDP-Dataset.R
+++ IHDP-Dataset/IHDP-Dataset.R
@@ -159,9 +159,10 @@ for(h in 1:nreps) {
   hvalue_rf[h] <- ncv_rf$hvalue
   cat("Simulation Replication: ", h, "\n")
 }
-## Save: cover_lm, cover_glmnet, cover_glmboost, cover_bart
-##       CI_lm, CI_glmnet, CI_glmboost, CI_bart
-##      hvalue_lm, hvalue_glmnet, hvalue_glmboost, hvalue_bart
+
+save( CI_lm, CI_glmnet, CI_glmboost, CI_rf,
+      hvalue_lm, hvalue_glmnet, hvalue_glmboost, hvalue_rf,
+      file = "IHDP_nrep_500.RData")
 
 
 # histogram - h-values
